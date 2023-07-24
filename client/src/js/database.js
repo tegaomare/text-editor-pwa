cfb2303fc5f261773a594444435987d018640449
tegaomare <tegaomare@yahoo.com>
2023-07-24 19:23:31 -0400

    database connection for get added,new transaction, getall method & request confirmation added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,6 +28,23 @@ const request = store.put({ id: 1, value: content });
 const result = await request;
 console.log("🚀 - data saved to the database", result);
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.log("GET from the database");
+export const getDb = async () => {
+  console.log("GET from the database");
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB("jate", 1);
 
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+};
 initdb();
