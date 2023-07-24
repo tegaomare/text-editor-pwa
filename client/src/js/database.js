5aa353c104998be96d86aea7ed07086396a836ea
tegaomare <tegaomare@yahoo.com>
2023-07-24 19:15:05 -0400

    created a new transaction database & desird object store

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,11 @@ const initdb = async () =>
 export const putDb = async (content) => console.log("PUT to the database");
 // Create a connection to the database database and version we want to use.
 const contactDb = await openDB("jate", 1);
+// Create a new transaction and specify the database and data privileges.
+const tx = contactDb.transaction("jate", "readwrite");
 
+// Open up the desired object store.
+const store = tx.objectStore("jate");
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error("getDb not implemented");
 
