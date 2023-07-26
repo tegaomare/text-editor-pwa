313ba45c2955f4084775e0f1ad1c968e2f7233fb
tegaomare <tegaomare@yahoo.com>
2023-07-26 18:54:38 -0400

    services worker bug fixed

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,9 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
-
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  await openDB("jate", 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
@@ -11,22 +11,24 @@ const initdb = async () =>
       console.log("jate database created");
     },
   });
-
+initdb();
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.log("PUT to the database");
-// Create a connection to the database database and version we want to use.
-const contactDb = await openDB("jate", 1);
-// Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction("jate", "readwrite");
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction("jate", "readwrite");
 
-// Open up the desired object store.
-const store = tx.objectStore("jate");
-// Use the .add() method on the store and pass in the content.
-const request = store.put({ id: 1, value: content });
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
 
-// Get confirmation of the request.
-const result = await request;
-console.log("🚀 - data saved to the database", result);
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
@@ -47,4 +49,3 @@ export const getDb = async () => {
   console.log("result.value", result);
   return result?.value;
 };
-initdb();
